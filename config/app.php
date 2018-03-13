24e80c4ced93c92c249283d01d34680382efe902
KevalGondaliya <keval.gondaliya.km@gmail.com>
2018-03-14 00:10:12 +0530

    image functinality complete

--- config/app.php
+++ config/app.php
@@ -146,7 +146,9 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+        Intervention\Image\ImageServiceProvider::class,
+        Illuminate\Html\HtmlServiceProvider::class, 
+        Illuminate\Html\HtmlServiceProvider::class, 
         /*
          * Package Service Providers...
          */
@@ -159,6 +161,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
 
     ],
 
@@ -208,6 +211,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Image' => Intervention\Image\Facades\Image::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h1>This is the testing page</h1>
+</body>
+</html>
