262084c7192f481e5b74aadfdf131294c6a22762
KevalGondaliya <keval.gondaliya.km@gmail.com>
2018-03-16 00:23:39 +0530

    complete convert image

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -17,10 +17,9 @@ class ImageController extends Controller
     {
         $this->validate($request, [
             'photo' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:1536',
-        ]);
+        ]); 
         $photo = $request->file('photo');
         $imagename = time().'.'.$photo->getClientOriginalExtension(); 
-
 
         // icon for android 
 
@@ -51,7 +50,126 @@ class ImageController extends Controller
         $destinationPath = public_path('upload/android/');
         $thumb_img = Image::make($photo->getRealPath())->resize(256, 256);
         $thumb_img->save($destinationPath.'/'.$imagename);
+
+
+        // iphone
+        // i20 --------------------------------------------
+
+        $destinationPath = public_path('upload/iphone/i20/Icon-App-20x20@2x/');
+        $thumb_img = Image::make($photo->getRealPath())->resize(100, 100);
+        $thumb_img->save($destinationPath.'/'.$imagename);
+
+        $destinationPath = public_path('upload/iphone/i20/Icon-App-20x20@3x/');
+        $thumb_img = Image::make($photo->getRealPath())->resize(60, 60);
+        $thumb_img->save($destinationPath.'/'.$imagename);
+
+         $destinationPath = public_path('upload/iphone/i29/Icon-App-29x29@1x/');
+        $thumb_img = Image::make($photo->getRealPath())->resize(29, 29);
+        $thumb_img->save($destinationPath.'/'.$imagename);
+
+        // i29-------------------------------------------
+
+         $destinationPath = public_path('upload/iphone/i29/Icon-App-29x29@2x/');
+        $thumb_img = Image::make($photo->getRealPath())->resize(58, 58);
+        $thumb_img->save($destinationPath.'/'.$imagename);
+
+         $destinationPath = public_path('upload/iphone/i29/Icon-App-29x29@3x/');
+        $thumb_img = Image::make($photo->getRealPath())->resize(87, 87);
+        $thumb_img->save($destinationPath.'/'.$imagename);
         
+        // i40-----------------------------------------
+
+         $destinationPath = public_path('upload/iphone/i40/Icon-App-40x40@1x/');
+        $thumb_img = Image::make($photo->getRealPath())->resize(40, 40);
+        $thumb_img->save($destinationPath.'/'.$imagename);
+
+         $destinationPath = public_path('upload/iphone/i40/Icon-App-40x40@2x/');
+        $thumb_img = Image::make($photo->getRealPath())->resize(80, 80);
+        $thumb_img->save($destinationPath.'/'.$imagename);
+
+
+         $destinationPath = public_path('upload/iphone/i40/Icon-App-40x40@3x/');
+        $thumb_img = Image::make($photo->getRealPath())->resize(120, 120);
+        $thumb_img->save($destinationPath.'/'.$imagename);
+    
+
+        // i57-----------------------------------------
+
+           $destinationPath = public_path('upload/iphone/i57/Icon-App-57x57@1x/');
+        $thumb_img = Image::make($photo->getRealPath())->resize(57, 57);
+        $thumb_img->save($destinationPath.'/'.$imagename);
+
+           $destinationPath = public_path('upload/iphone/i57/Icon-App-57x57@2x/');
+        $thumb_img = Image::make($photo->getRealPath())->resize(114, 114);
+        $thumb_img->save($destinationPath.'/'.$imagename);
+
+
+
+        // i60-----------------------------------------    
+
+          $destinationPath = public_path('upload/iphone/i60/Icon-App-60x60@1x/');
+        $thumb_img = Image::make($photo->getRealPath())->resize(60, 60);
+        $thumb_img->save($destinationPath.'/'.$imagename);
+
+          $destinationPath = public_path('upload/iphone/i60/Icon-App-60x60@2x/');
+        $thumb_img = Image::make($photo->getRealPath())->resize(120, 120);
+        $thumb_img->save($destinationPath.'/'.$imagename);
+
+          $destinationPath = public_path('upload/iphone/i60/Icon-App-60x60@3x/');
+        $thumb_img = Image::make($photo->getRealPath())->resize(180, 180);
+        $thumb_img->save($destinationPath.'/'.$imagename);
+
+
+        // i72----------------------------------------- 
+
+
+          $destinationPath = public_path('upload/iphone/i72/Icon-App-72x72@1x/');
+        $thumb_img = Image::make($photo->getRealPath())->resize(72, 72);
+        $thumb_img->save($destinationPath.'/'.$imagename);
+
+          $destinationPath = public_path('upload/iphone/i72/Icon-App-72x72@2x/');
+        $thumb_img = Image::make($photo->getRealPath())->resize(144, 144);
+        $thumb_img->save($destinationPath.'/'.$imagename);
+
+
+        // i76----------------------------------------- 
+
+         $destinationPath = public_path('upload/iphone/i76/Icon-App-76x76@1x/');
+        $thumb_img = Image::make($photo->getRealPath())->resize(76, 76);
+        $thumb_img->save($destinationPath.'/'.$imagename);
+
+         $destinationPath = public_path('upload/iphone/i76/Icon-App-76x76@2x/');
+        $thumb_img = Image::make($photo->getRealPath())->resize(152, 152);
+        $thumb_img->save($destinationPath.'/'.$imagename);
+
+         $destinationPath = public_path('upload/iphone/i76/Icon-App-76x76@3x/');
+        $thumb_img = Image::make($photo->getRealPath())->resize(228, 228);
+        $thumb_img->save($destinationPath.'/'.$imagename);
+
+
+        // i83.5----------------------------------------- 
+
+        $destinationPath = public_path('upload/iphone/i83.5/Icon-App-83.5x83.5@2x/');
+        $thumb_img = Image::make($photo->getRealPath())->resize(167, 167);
+        $thumb_img->save($destinationPath.'/'.$imagename);
+
+
+        // ismall50----------------------------------------- 
+
+        $destinationPath = public_path('upload/iphone/ismall50/Icon-Small-50x50@1x/');
+        $thumb_img = Image::make($photo->getRealPath())->resize(50, 50);
+        $thumb_img->save($destinationPath.'/'.$imagename);
+
+        $destinationPath = public_path('upload/iphone/ismall50/Icon-Small-50x50@2x/');
+        $thumb_img = Image::make($photo->getRealPath())->resize(100, 100);
+        $thumb_img->save($destinationPath.'/'.$imagename);
+
+         // iphone----------------------------------------- 
+
+        $destinationPath = public_path('upload/iphone/istore/');
+        $thumb_img = Image::make($photo->getRealPath())->resize(1024, 1024);
+        $thumb_img->save($destinationPath.'/'.$imagename);
+
 
         $destinationPath = public_path('upload/');
         $photo->move($destinationPath, $imagename);
--- resources/views/resizeImage.blade.php
+++ resources/views/resizeImage.blade.php
@@ -20,6 +20,9 @@
 	            <img src="{{asset('upload/small/'.Session::get('imagename')) }}" />
 	        </div>
         </div> -->
+
+        <!-- android icon -->
+
         <div class="col-md-12" style="margin-top:10px;">
 	        <div class="col-md-4">
 	            <p style="margin-left: 15px; display: inline-flex;">ic_launcher</p>
@@ -27,6 +30,7 @@
 	        <div class="col-md-8">    
 	            <img src="{{asset('upload/android/mipmap-hdpi/'.Session::get('imagename')) }}" />
 	        </div>
+	        
         </div>
         <div class="col-md-12" style="margin-top:10px;">
 	        <div class="col-md-4">
@@ -76,6 +80,211 @@
 	            <img src="{{asset('upload/android/'.Session::get('imagename')) }}" />
 	        </div>
         </div>
+
+
+
+        <!-- iphone icon -->
+
+
+        <!--  i20 icon-->
+
+        <div class="col-md-12" style="margin-top:10px;">
+	        <div class="col-md-4">
+	            <p style="margin-left: 15px;">Icon-App-20x20@2x</p>
+	        </div>
+	        <div class="col-md-8">    	
+	            <img src="{{asset('upload/iphone/i20/Icon-App-20x20@2x/'.Session::get('imagename')) }}" />
+	        </div>
+        </div>
+
+        <div class="col-md-12" style="margin-top:10px;">
+	        <div class="col-md-4">
+	            <p style="margin-left: 15px;">Icon-App-20x20@3x</p>
+	        </div>
+	        <div class="col-md-8">    
+	            <img src="{{asset('upload/iphone/i20/Icon-App-20x20@3x/'.Session::get('imagename')) }}" />
+	        </div>
+        </div>
+
+        <div class="col-md-12" style="margin-top:10px;">
+	        <div class="col-md-4">
+	            <p style="margin-left: 15px;">Icon-App-29x29@1x</p>
+	        </div>
+	        <div class="col-md-8">    
+	            <img src="{{asset('upload/iphone/i29/Icon-App-29x29@1x/'.Session::get('imagename')) }}" />
+	        </div>
+        </div>
+
+        <!--  i40 icon-->
+
+        <div class="col-md-12" style="margin-top:10px;">
+	        <div class="col-md-4">
+	            <p style="margin-left: 15px;">Icon-App-40x40@1x</p>
+	        </div>
+	        <div class="col-md-8">    
+	            <img src="{{asset('upload/iphone/i40/Icon-App-40x40@1x/'.Session::get('imagename')) }}" />
+	        </div>
+        </div>
+
+        <div class="col-md-12" style="margin-top:10px;">
+	        <div class="col-md-4">
+	            <p style="margin-left: 15px;">Icon-App-40x40@2x</p>
+	        </div>
+	        <div class="col-md-8">    
+	            <img src="{{asset('upload/iphone/i40/Icon-App-40x40@2x/'.Session::get('imagename')) }}" />
+	        </div>
+        </div>
+
+        <div class="col-md-12" style="margin-top:10px;">
+	        <div class="col-md-4">
+	            <p style="margin-left: 15px;">Icon-App-40x40@3x</p>
+	        </div>
+	        <div class="col-md-8">    
+	            <img src="{{asset('upload/iphone/i40/Icon-App-40x40@3x/'.Session::get('imagename')) }}" />
+	        </div>
+        </div>
+
+        <!--  i57 icon-->
+
+        <div class="col-md-12" style="margin-top:10px;">
+	        <div class="col-md-4">
+	            <p style="margin-left: 15px;">Icon-App-57x57@1x</p>
+	        </div>
+	        <div class="col-md-8">    
+	            <img src="{{asset('upload/iphone/i57/Icon-App-57x57@1x/'.Session::get('imagename')) }}" />
+	        </div>
+        </div>
+
+        <div class="col-md-12" style="margin-top:10px;">
+	        <div class="col-md-4">
+	            <p style="margin-left: 15px;">Icon-App-57x57@2x</p>
+	        </div>
+	        <div class="col-md-8">    
+	            <img src="{{asset('upload/iphone/i57/Icon-App-57x57@2x/'.Session::get('imagename')) }}" />
+	        </div>
+        </div>
+         <!--  i60 icon-->
+
+        <div class="col-md-12" style="margin-top:10px;">
+	        <div class="col-md-4">
+	            <p style="margin-left: 15px;">Icon-App-60x60@1x</p>
+	        </div>
+	        <div class="col-md-8">    
+	            <img src="{{asset('upload/iphone/i60/Icon-App-60x60@1x/'.Session::get('imagename')) }}" />
+	        </div>
+        </div>
+
+        <div class="col-md-12" style="margin-top:10px;">
+	        <div class="col-md-4">
+	            <p style="margin-left: 15px;">Icon-App-60x60@2x</p>
+	        </div>
+	        <div class="col-md-8">    
+	            <img src="{{asset('upload/iphone/i60/Icon-App-60x60@2x/'.Session::get('imagename')) }}" />
+	        </div>
+        </div>
+
+          <div class="col-md-12" style="margin-top:10px;">
+	        <div class="col-md-4">
+	            <p style="margin-left: 15px;">Icon-App-60x60@3x</p>
+	        </div>
+	        <div class="col-md-8">    
+	            <img src="{{asset('upload/iphone/i60/Icon-App-60x60@3x/'.Session::get('imagename')) }}" />
+	        </div>
+        </div>
+
+          <!--  i72 icon-->
+
+        <div class="col-md-12" style="margin-top:10px;">
+	        <div class="col-md-4">
+	            <p style="margin-left: 15px;">Icon-App-72x72@1x</p>
+	        </div>
+	        <div class="col-md-8">    
+	            <img src="{{asset('upload/iphone/i72/Icon-App-72x72@1x/'.Session::get('imagename')) }}" />
+	        </div>
+        </div>
+
+        <div class="col-md-12" style="margin-top:10px;">
+	        <div class="col-md-4">
+	            <p style="margin-left: 15px;">Icon-App-72x72@2x</p>
+	        </div>
+	        <div class="col-md-8">    
+	            <img src="{{asset('upload/iphone/i72/Icon-App-72x72@2x/'.Session::get('imagename')) }}" />
+	        </div>
+        </div>
+
+         <!--  i76 icon-->
+
+        <div class="col-md-12" style="margin-top:10px;">
+	        <div class="col-md-4">
+	            <p style="margin-left: 15px;">Icon-App-76x76@1x</p>
+	        </div>
+	        <div class="col-md-8">    
+	            <img src="{{asset('upload/iphone/i76/Icon-App-76x76@1x/'.Session::get('imagename')) }}" />
+	        </div>
+        </div>
+
+        <div class="col-md-12" style="margin-top:10px;">
+	        <div class="col-md-4">
+	            <p style="margin-left: 15px;">Icon-App-76x76@2x</p>
+	        </div>
+	        <div class="col-md-8">    
+	            <img src="{{asset('upload/iphone/i76/Icon-App-76x76@2x/'.Session::get('imagename')) }}" />
+	        </div>
+        </div>
+
+       <div class="col-md-12" style="margin-top:10px;">
+	        <div class="col-md-4">
+	            <p style="margin-left: 15px;">Icon-App-76x76@3x</p>
+	        </div>
+	        <div class="col-md-8">    
+	            <img src="{{asset('upload/iphone/i76/Icon-App-76x76@3x/'.Session::get('imagename')) }}" />
+	        </div>
+        </div>
+
+           <!--  i83.5 icon-->
+
+        <div class="col-md-12" style="margin-top:10px;">
+	        <div class="col-md-4">
+	            <p style="margin-left: 15px;">Icon-App-83.5x83</p>
+	        </div>
+	        <div class="col-md-8">    
+	            <img src="{{asset('upload/iphone/i83.5/Icon-App-83.5x83.5@2x/'.Session::get('imagename')) }}" />
+	        </div>
+        </div>
+
+          <!--  ismall50 icon-->
+
+        <div class="col-md-12" style="margin-top:10px;">
+	        <div class="col-md-4">
+	            <p style="margin-left: 15px;">Icon-Small-50x50@1x</p>
+	        </div>
+	        <div class="col-md-8">    
+	            <img src="{{asset('upload/iphone/ismall50/Icon-Small-50x50@1x/'.Session::get('imagename')) }}" />
+	        </div>
+        </div>
+
+
+        <div class="col-md-12" style="margin-top:10px;">
+	        <div class="col-md-4">
+	            <p style="margin-left: 15px;">Icon-Small-50x50@2x</p>
+	        </div>
+	        <div class="col-md-8">    
+	            <img src="{{asset('upload/iphone/ismall50/Icon-Small-50x50@2x/'.Session::get('imagename')) }}" />
+	        </div>
+        </div>
+
+
+          <!--  iphone icon-->
+
+        <div class="col-md-12" style="margin-top:10px;">
+	        <div class="col-md-4">
+	            <p style="margin-left: 15px;">ItunesArtwork@2x</p>
+	        </div>
+	        <div class="col-md-8">    
+	            <img src="{{asset('upload/iphone/istore/'.Session::get('imagename')) }}" />
+	        </div>
+        </div>
+
         
         
     </div>
